a653aabec18959f3d9d40c4dc7be88dc09a951a5
Oleksii Horban <lehagorban93@gmail.com>
2023-08-03 18:05:59 +0300

    fix

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,7 +1,9 @@
 import { FC } from "react";
-import { Swiper, SwiperClass } from "swiper/react";
+import { Swiper } from "swiper/react";
+
 import "swiper/css";
 import "swiper/css/navigation";
+
 import { Navigation } from "swiper/modules";
 
 import { SliderProps } from "./Slider.props";
@@ -18,46 +20,105 @@ const Slider: FC<SliderProps> = ({
   btnChildrenLeft,
   btnChildrenRight,
 }) => {
-  const swiperOptions: SwiperClass = {
-    slidesPerView: slide,
-    spaceBetween: space,
-    navigation: {
-      prevEl: document.querySelector("." + prevBtn),
-      nextEl: document.querySelector("." + nextBtn),
-    },
-
-    modules: [Navigation],
-
-    breakpoints: {
-      120: {
-        slidesPerView: 1,
-        spaceBetween: space,
-      },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: space,
-      },
-      1312: {
-        slidesPerView: slide,
-        spaceBetween: space,
-      },
-    },
-  };
-
   return (
-    <div className="relative">
-      <Swiper className={`mySwiper ${className} `} {...swiperOptions}>
-        {children}
-      </Swiper>
+    <>
+      <div className="relative">
+        <Swiper
+          slidesPerView={slide}
+          spaceBetween={space}
+          navigation={{
+            prevEl: "." + prevBtn,
+            nextEl: "." + nextBtn,
+          }}
+          modules={[Navigation]}
+          className={`mySwiper ${className} `}
+          breakpoints={{
+            120: {
+              slidesPerView: 1,
+            },
+            768: {
+              slidesPerView: 2,
+            },
+            1312: {
+              slidesPerView: slide,
+            },
+          }}
+        >
+          {children}
+        </Swiper>
 
-      <button className={`${prevBtn} ${prevBtnStyle}`}>
-        {btnChildrenLeft}
-      </button>
-      <button className={`${nextBtn} ${nextBtnStyle}`}>
-        {btnChildrenRight}
-      </button>
-    </div>
+        <button className={`${prevBtn} ${prevBtnStyle}`}>
+          {btnChildrenLeft}
+        </button>
+        <button className={`${nextBtn} ${nextBtnStyle}`}>
+          {btnChildrenRight}
+        </button>
+      </div>
+    </>
   );
 };
 
 export default Slider;
+
+// import { FC } from "react";
+// import { Swiper, SwiperClass } from "swiper/react";
+// import "swiper/css";
+// import "swiper/css/navigation";
+// import { Navigation } from "swiper/modules";
+
+// import { SliderProps } from "./Slider.props";
+
+// const Slider: FC<SliderProps> = ({
+//   children,
+//   className,
+//   slide,
+//   space,
+//   prevBtn,
+//   nextBtn,
+//   prevBtnStyle,
+//   nextBtnStyle,
+//   btnChildrenLeft,
+//   btnChildrenRight,
+// }) => {
+//   const swiperOptions: SwiperClass = {
+//     slidesPerView: slide,
+//     spaceBetween: space,
+//     navigation: {
+//       prevEl: "." + prevBtn,
+//       nextEl: "." + nextBtn,
+//     },
+//     modules: [Navigation],
+
+//     breakpoints: {
+//       120: {
+//         slidesPerView: 1,
+//         spaceBetween: space,
+//       },
+//       768: {
+//         slidesPerView: 2,
+//         spaceBetween: space,
+//       },
+//       1312: {
+//         slidesPerView: slide,
+//         spaceBetween: space,
+//       },
+//     },
+//   };
+
+//   return (
+//     <div className="relative">
+//       <Swiper className={`mySwiper ${className} `} {...swiperOptions}>
+//         {children}
+//       </Swiper>
+
+//       <button className={`${prevBtn} ${prevBtnStyle}`}>
+//         {btnChildrenLeft}
+//       </button>
+//       <button className={`${nextBtn} ${nextBtnStyle}`}>
+//         {btnChildrenRight}
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default Slider;
